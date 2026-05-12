eaa6c3c529e6f4b38d2d79d43948918debf57449
marcos-j-lemes <marcos.jlf@aluno.ifsc.edu.br>
2026-05-12 19:47:28 -0300

    feat: configuração inicial pronta

--- refatoracao/src/main/java/com/eng/refatoracao/Main.java
+++ refatoracao/src/main/java/com/eng/refatoracao/Main.java
@@ -1,9 +1,26 @@
 package com.eng.refatoracao;
 
-
 public class Main {
-    
-    public static void main(String[] args){
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+
+        Pedido p = new Pedido();
+
+        p.clienteNome = "Maria";
+        p.clienteEmail = "maria@email.com";
+        p.clienteEndereco = "SC";
+
+        p.adicionarItem("Notebook", 3000, 1);
+        p.adicionarItem("Mouse", 100, 2);
+
+        p.finalizar();
+
+        BancoDeDados.salvarLog("Sistema finalizado");
+
+        RelatorioService r = new RelatorioService();
+        r.gerar(p);
+
+        System.out.println("Frete: " + p.frete);
+        System.out.println("Status: " + p.status);
     }
 }
---
+++ refatoracao/src/main/java/com/eng/refatoracao/RelatorioService.java
@@ -0,0 +1,14 @@
+package com.eng.refatoracao;
+
+
+public class RelatorioService {
+
+    public void gerar(Pedido p) {
+        System.out.println("Cliente: " + p.clienteNome);
+        System.out.println("Total com frete: " + (p.total + p.frete));
+
+        if (p.total > 1000) {
+            System.out.println("Cliente VIP");
+        }
+    }
+}
