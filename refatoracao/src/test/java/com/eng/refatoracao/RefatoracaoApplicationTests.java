378a1c7ef11a3129a326abe37ab64b985767df33
marcos-j-lemes <marcos.jlf@aluno.ifsc.edu.br>
2026-05-12 19:37:40 -0300

    feat: settings initial

---
+++ refatoracao/src/test/java/com/eng/refatoracao/RefatoracaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eng.refatoracao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefatoracaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
